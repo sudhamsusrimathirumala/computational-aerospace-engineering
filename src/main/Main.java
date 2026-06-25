5a9122cabbec1244ca8110de32e21b2ff6d508bb
Sudhamsu <academyoffice79@gmail.com>
2026-06-24 22:47:45 -0400

    Java environment and Gradle Setup

--- src/main/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Computational Aerospace Engineering");
-        
-        ProjectileSim sim = new ProjectileSim(10,45,0.1);
-        sim.runSim();
-    }
-}
---
+++ src/main/java/com/sudhamsu/aerospace/Main.java
@@ -0,0 +1,7 @@
+package java.com.sudhamsu.aerospace;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Computational Space Mission Analysis Platform");
+    }
+}
